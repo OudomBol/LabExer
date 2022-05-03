efb446f234a11f5114cdc780b4790734c6d415bd
OudomBol <100819122+OudomBol@users.noreply.github.com>
2022-05-03 21:32:36 +0700

    Add files via upload

--- port.cpp
+++ port.cpp
@@ -2,22 +2,10 @@
 using namespace std;          //which namespace to use
 
 //ASSUME THEY HAVEN’T TAKEN ANY RAPID TEST
- //Press sth to start again (loop?). Press sth to end. ?
- //If you need to start over, refresh the page and start again. ?
 
 
-//DONT HAVE INVALID INPUT YET: DONE
-//no loop yet 
-//more function: 
-//add array
-//DISPLAY: Your results: DONE
 
-//add comments
-
-//understood the importance of function, had to edit the code lots of time, using function really helps
-
-//these are functions we used
-char choice;
+//these are functions used
 void introduction()
 {
      cout << "\n";                                         //this block of code is the introduction to the program
@@ -31,7 +19,27 @@ void introduction()
      cout << "\n The purpose of this program is to help you make decisions about seeking appropriate medical care. "<<endl;
      cout <<" It is NOT intended for self-diagnosis or treatment of COVID-19. ";
      cout << "\n ***** PRESS ENTER TO CONTINUE ***** ";         
+     cin.get(); // wait for the user input before the program can move forward
+}
+
+void advisor()                     //give advice on what to do at all times during the pandemic
+{
+     cout << "\n You are recommended to follow these steps at all times to reduce your risk of getting or spreading COVID-19: "<<endl;
+     cout << " - Get COVID-19 vaccines if you haven't. "<<endl;
+     cout << " - Wear a mask when you're outside. "<<endl;
+     cout << " - Practice social distancing and avoid poorly ventilated spaces. "<<endl;
+     cout << " - Wash your hands often with soap or use hand sanitizer. "<<endl;
+}
+
+void presstocont()
+{
+     cout << "\n ***** PRESS ENTER TO CONTINUE ***** ";         
      cin.get();
+}
+
+void askuserinputyn ()        //ask user to enter yes or no
+{
+     cout << "\n Enter Y for YES or N for NO: ";
 }
 
 void lifethreateningsymptomsQ()     //asking user if they have life threatening symptoms
@@ -46,29 +54,21 @@ void  lifethreateningsymptoms()    //recommendation if they have life-threatenin
 {
      cout << "\n Based on your symptoms, you may need urgent medical care.";
      cout << "\n Please visit the nearest medical center."<<endl;
-     //cout << "\n Enter A to start again or X to exit the program: "; //testing
-     //cin>>choice;
 }
 
-void advisor()                     //give advice on what to do at all times during the pandemic
+void askuserinputynm ()       //ask user to enter yes, no, or not sure
 {
-     cout << "\n You are recommended to follow these steps at all times to reduce your risk of getting or spreading COVID-19: "<<endl;
-     cout << " - Get COVID-19 vaccines if you haven't. "<<endl;
-     cout << " - Wear a mask when you're outside. "<<endl;
-     cout << " - Practice social distancing and avoid poorly ventilated spaces. "<<endl;
-     cout << " - Wash your hands often with soap or use hand sanitizer. "<<endl;
+     cout << " In the last two weeks, have you been in close contact with someone who has COVID-19? "<<endl;
+     cout << " Enter Y for YES, or N for NO, or M for NOT SURE: ";   //yes, no or idk
 }
 
-void symptoms()                    //symptoms of covid-19
+void printSymptoms (string array[], int size) //this is for passing array to function
 {
-     cout << "\n In the last 10 days, have you experienced ANY of the symptoms listed below? "<<endl;
-     cout << " - Fever "<<endl;
-     cout << " - Cough/Sore throat "<<endl;
-     cout << " - Fatigue "<<endl;
-     cout << " - Loss of taste or smell "<<endl;
-     cout << " - Difficulty breathing "<<endl;
-     cout << " - Runny nose "<<endl;
-     cout << " - Headache "<<endl;
+    cout << "\n In the last 10 days, have you experienced ANY of the symptoms listed below? "<<endl;
+    for ( int i=0; i<size; i++)
+    {
+      cout << array[i] <<endl;
+    }
 }
 
 void vaxhavesymptoms()            //for people with COVID-19 vaccine and have symptoms
@@ -77,6 +77,31 @@ void vaxhavesymptoms()            //for people with COVID-19 vaccine and have sy
      cout << " - Isolate yourself at home and take care of yourself. "<<endl;
      cout << " - Continue wearing a mask around others until you are able to get tested and receive results of COVID-19. "<<endl;
      cout << "\n PLEASE NOTE: Because some of the symptoms of flu and COVID-19 are similar, "<<endl<< "you may want to consider asking the doctor about testing or treatment for influenza."<<endl;
+}
+
+void contactbutnosymptoms()      //for people that made contact with an infected person but got no symptoms
+{
+     cout << "\n You do not need to quarantine. But you should take precautions until 10 days following close contact as follows: "<<endl;
+     cout << " - Wear a well-fitting mask when you are around others. "<<endl;
+     cout << " - Avoid places where you are not able to wear a mask and avoid travelling. "<<endl;
+}
+
+void invalidin()    
+{
+    cout << "\n";
+    cout <<"\t INVALID INPUT ! PLEASE ENTER THE RIGHT LETTER THIS TIME \n";
+}
+
+void nocontactnosymptoms()    //for people that didn't make contact with an infected person and got no symptoms
+{
+     cout << "\n Sounds like you are feeling ok. You can stop stressing about it. "<<endl;
+}
+
+void notsureandnosymptoms()     //for people that are not sure they made contact with an infected person and got no symptoms
+{
+     cout << "\n You do not need to quarantine. But you should still take precautions as follows: "<<endl;
+     cout << " - Wear a well-fitting mask when you are around others. "<<endl;
+     cout << " - Avoid places where you are not able to wear a mask and avoid travelling. "<<endl;
 }
 
 void novaxhavesymptoms()          //for people with no COVID-19 vaccine and have symptoms
@@ -90,43 +115,8 @@ void novaxhavesymptoms()          //for people with no COVID-19 vaccine and have
 
 void nosymptomsnovaccine()       //for people with no COVID-19 vaccine and no symptoms
 {
-     cout << "\n Watch out for COVID-19 symptoms such as fever, cough, or difficulty breathing."<<endl;
+     cout << "\n Because you're unvaccinated, be on the look out for COVID-19 symptoms such as fever, cough, or difficulty in breathing."<<endl;
      cout << "If you develop any of these symptoms or if you start feeling worse, visit the nearest medical center. "<<endl;
-}
-
-void contactbutnosymptoms()      //for people that made contact with an infected person but got no symptoms
-{
-     cout << "\n You do not need to quarantine. But you should take precautions until 10 days following close contact as follows: "<<endl;
-     cout << " - Wear a well-fitting mask when you are around others. "<<endl;
-     cout << " - Avoid places where you are not able to wear a mask and avoid travelling. "<<endl;
-}
-
-void notsureandnosymptoms()     //for people that are not sure they made contact with an infected person and got no symptoms
-{
-     cout << "\n You do not need to quarantine. But you should still take precautions as follows: "<<endl;
-     cout << " - Wear a well-fitting mask when you are around others. "<<endl;
-     cout << " - Avoid places where you are not able to wear a mask and avoid travelling. "<<endl;
-}
-
-void nocontactnosymptoms()    //for people that didn't make contact with an infected person and got no symptoms
-{
-     cout << "\n Sounds like you are feeling ok. You can stop stressing about it. "<<endl;
-}
-
-void askuserinputyn ()        //ask user to enter yes or no
-{
-     cout << "\n Enter Y for YES or N for NO: ";
-}
-
-void askuserinputynm ()       //ask user to enter yes, no, or not sure
-{
-     cout << " In the last two weeks, have you been in close contact with someone who has COVID-19? "<<endl;
-     cout << " Enter Y for YES, or N for NO, or M for NOT SURE: ";   //yes, no or idk
-}
-
-void invalidin()    
-{
-     cout <<"\n INVALID INPUT! PLEASE ENTER THE RIGHT LETTER"<<endl;
 }
 
 void result()          
@@ -134,59 +124,56 @@ void result()
      cout << "\n ***** YOUR RESULT ***** "<<endl;
 }
 
-void presstocont()
-{
-     cout << "\n ***** PRESS ENTER TO CONTINUE ***** ";         
-     cin.get();
-}
+
 
 //the main function
 int main()
 {    
  
- introduction();    //calling the introduction menu function
+//  introduction();    //calling the introduction menu function
 
- cout << "\n ***** BEFORE WE GET STARTED ***** "; 
- advisor();                                           //calling function that gives advice 
- presstocont();
+//  cout << "\n ***** BEFORE WE GET STARTED ***** "; 
+//  advisor();                                           //calling function that gives advice 
+//  presstocont();
 
-// while (toupper(choice) != 'X')
-// {
 
 //start switch from here
-while(1)
+while(1)       //infinite while loop 
 {
+ string sympt[10] = {" - Fever ", " - Cough/Sore throat ", " - Fatigue ", " - Loss of taste or smell ", 
+                    " - Difficulty breathing ", " - Runny nose ", " - Headache "};
  char choice;
  int option;
 
- cout << "\n ***** LET'S GET STARTED ***** "; 
- cout<<"\n [1] to Start the Program \n";
+ cout << "\n ***** SELECT ONE OF THE OPTIONS BELOW ***** "; 
+ cout<<"\n [1] to Start the Program \n";         //asking user to input the number 1 or 0
  cout<<" [0] to Exit the Program \n";
  cout<<"\n Your Choice: ";
  cin>>option;
 
  switch (option)
  {
-   case 0:
+   case 0:          //if they input 0, the program ends
    cout << "\n ***** Thank you for using this program ***** ";
    return 0;
 
    case 1: 
- cout << "\n Have you taken the recommeded dose of COVID-19 vaccine? ";
- askuserinputyn();                                     //calling the function that ask user for yes or no
- cin >> choice;                                        //for user to input their choice
+   cout << "\n Have you taken the recommeded dose of COVID-19 vaccine? ";
+   askuserinputyn();                                     //calling the function that ask user for yes or no
+   cin >> choice;                                        //for user to input their choice
  
  if (toupper(choice) == 'Y')  //toupper used for turn lowercase letters into uppercase
  {
      lifethreateningsymptomsQ();           //call function: that ask user if they have life threatening symptoms
      askuserinputyn();
-     cin >> choice;                         
+     cin >> choice;
+     cin.ignore();      //clear the previously entered choice so the cin.get() can work     
      
      if (toupper(choice) == 'Y')  
      {
           result();                     
           lifethreateningsymptoms();      //call function: that recommends what they should do if they have life-threatening symptoms
-
+          presstocont(); 
      }
 
      else if (toupper(choice) == 'N')
@@ -197,20 +184,23 @@ while(1)
 
           if (toupper(choice)== 'Y')  
           {
-               symptoms(); //calling the function for symptoms
+               printSymptoms(sympt, 7);  //calling the array function for symptoms
                askuserinputyn();
                cin>>choice;
+               cin.ignore();
 
                if (toupper(choice) == 'Y') 
                {
                     result();
                     vaxhavesymptoms();  //call function:for people with COVID-19 vaccine and have symptoms
+                    presstocont(); //
                }
 
                else if (toupper(choice) == 'N')
                {
                     result();
                     contactbutnosymptoms(); //call function:for people that made contact with an infected person but got no symptoms
+                    presstocont(); //
                }
                
                 else
@@ -221,19 +211,22 @@ while(1)
           }
           else if (toupper(choice) == 'N')
           {
-               symptoms();
+               printSymptoms(sympt, 7);
                askuserinputyn();
                cin>>choice;
+               cin.ignore();
 
                if (toupper(choice) == 'Y')  
                {
                     result();
                     vaxhavesymptoms();
+                     presstocont();
                }
                else if (toupper(choice) == 'N')
                {
                    result();
                    nocontactnosymptoms();  //call function:for people that didn't make contact with an infected person and got no symptoms
+                   presstocont();
                }
                else
                {
@@ -243,19 +236,22 @@ while(1)
           }
           else if (toupper(choice) == 'M')
           {
-               symptoms();
+               printSymptoms(sympt, 7);
                askuserinputyn();
                cin>>choice;
+               cin.ignore();
 
                if (toupper(choice) == 'Y')  
                {
                     result();
                     vaxhavesymptoms();
+                    presstocont();
                }
                else if(toupper(choice) == 'N')
                {
                     result();
                     notsureandnosymptoms();   //call function:for people that are not sure they made contact with an infected person and got no symptoms
+                    presstocont();
                }
                else
                {
@@ -266,15 +262,7 @@ while(1)
           {
                invalidin();
           }
-     } //test below here
-     // else if(toupper(choice) == 'A')
-     // {
-     //      break;
-     // }
-     // else if(toupper(choice) != 'Y' or toupper(choice) != 'N' or toupper(choice) != 'A')
-     // {
-     //      invalidin();
-     // }
+     } 
      else
      {
           invalidin();
@@ -286,12 +274,13 @@ while(1)
      lifethreateningsymptomsQ();
      askuserinputyn();
      cin>>choice;
+     cin.ignore();
 
      if (toupper(choice) == 'Y')  
      {
           result();
           lifethreateningsymptoms();
-          //advisor();
+          presstocont(); // heree
      }
      
      else if (toupper(choice) == 'N')
@@ -309,21 +298,22 @@ while(1)
                if(toupper(choice) == 'Y')   
                {
                     result();
-                    symptoms();
+                    printSymptoms(sympt, 7);
                     askuserinputyn();
                     cin>>choice;
+                      cin.ignore();
 
                     if (toupper(choice) == 'Y')  
                     {
                          result();
                          novaxhavesymptoms(); //call function:for people with no COVID-19 vaccine and have symptoms
-                         //advisor();
+                          presstocont();
                     }
                     else if (toupper(choice) == 'N')
                     {
                          result();
                          nosymptomsnovaccine(); //call function:for people with no COVID-19 vaccine and no symptoms
-                         //advisor();
+                          presstocont();
                     }
                     else
                     {
@@ -332,21 +322,22 @@ while(1)
                }
                else if (toupper(choice) == 'N')
                {
-                    symptoms();
+                    printSymptoms(sympt, 7);
                     askuserinputyn();
                     cin>>choice;
+                      cin.ignore();
 
                     if (toupper(choice) == 'Y')  
                     {
                          result();
                          novaxhavesymptoms(); 
-                         //advisor();
+                          presstocont();
                     }
                      else if (toupper(choice) == 'N')
                     {
                          result();
                          nosymptomsnovaccine();
-                         //advisor();
+                          presstocont();
                     }
                     else
                     {
@@ -355,21 +346,22 @@ while(1)
                }
                else if (toupper(choice) == 'M')
                {
-                    symptoms();
+                    printSymptoms(sympt, 7);
                     askuserinputyn();
                     cin>>choice;
+                      cin.ignore();
 
                     if (toupper(choice) == 'Y') 
                     {
                          result();
                          novaxhavesymptoms(); 
-                         //advisor();
+                          presstocont();
                     }
                      else if (toupper(choice) == 'N')
                     {
                          result();
                          nosymptomsnovaccine();
-                         //advisor();
+                          presstocont();
                     }
                     else
                     {
@@ -387,24 +379,25 @@ while(1)
                cout << "\n";
                askuserinputynm ();
                cin >> choice;
+                 cin.ignore();
 
                if (toupper(choice) == 'Y')   
                {
                     result();
                     nosymptomsnovaccine();
-                    //advisor();
+                     presstocont();
                }
                else if (toupper(choice) == 'N')
                {
                     result();
                     cout << "\n Based on the answers given, you do not need to get tested unless recommended or required. "<<endl;
-                    //advisor();
+                     presstocont();
                }
                else if (toupper(choice) == 'M')
                {
                     result();
                     nosymptomsnovaccine();
-                    //advisor();
+                     presstocont();
                }
                else
                {
@@ -421,51 +414,24 @@ while(1)
           invalidin();
      }
  }
- else
- {
+ else 
+{
      invalidin();
- }
- break;
-
-  default:
-  cout << "\n";
-  cout <<"\t INVALID INPUT ! PLEASE ENTER THE RIGHT NUMBER \n";
-
- }
 }
 
+ break; 
 
+ default:
+ cout << "\n";
+ cout <<"\t INVALID INPUT ! PLEASE ENTER THE RIGHT NUMBER \n";
 
- 
- //cout << "\n ***** Thank you for using this program ***** ";   //paste this everywhere
- //cout << "\n ***** Thank you "<<name<<" for using this program ***** ";
+ } //end of switch
 
+}  //end of while loop
 
      return 0;               //send 0 back to OS
 
 } // end of main
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
